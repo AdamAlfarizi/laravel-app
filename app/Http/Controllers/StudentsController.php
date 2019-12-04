8324438eda6324db39a1bc1efd4eedf4a1697f24
adam <adamalfal8@mail.unpas.ac.id>
2019-12-05 00:33:36 +0700

    tambah edit

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -74,7 +74,7 @@ class StudentsController extends Controller
      */
     public function edit(Student $student)
     {
-        //
+        return view('students.edit', compact('student'));
     }
 
     /**
@@ -86,7 +86,21 @@ class StudentsController extends Controller
      */
     public function update(Request $request, Student $student)
     {
-        //
+        $request->validate([
+            'nama' => 'required',
+            'nrp' => 'required|size:9',
+            'email' => 'required',
+            'jurusan' => 'required'
+        ]);
+        
+        Student::where('id', $student->id)
+        ->update([
+            'nama' => $request->nama,
+            'nrp' => $request->nrp,
+            'email' => $request->email,
+            'jurusan' => $request->jurusan
+        ]);
+        return redirect('/students')->with('status','Data Mahasiswa Berhasil Di Ubah!');
     }
 
     /**
